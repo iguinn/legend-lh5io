cde74cf0e4c65e7ae6ab3d126aa7d5ae666b2ce4
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2026-02-22 18:17:06 +0100

    fix: make vector_of_vectors read robust to empty idx and starts

--- tests/lh5/test_lh5_store.py
+++ tests/lh5/test_lh5_store.py
@@ -252,9 +252,19 @@ def test_read_vov_fancy_idx(lh5_file):
     lh5_obj = store.read("/data/struct_full/vov3d", lh5_file, idx=[0, 2])
     assert isinstance(lh5_obj, types.VectorOfVectors)
 
-    print(lh5_obj)
     assert lh5_obj == types.VectorOfVectors([[[1, 2], [3, 4, 5]], [[5, 3, 1]]])
     assert len(lh5_obj) == 2
+
+    # Out-of-range indices should be culled (and not raise).
+    lh5_obj = store.read("/data/struct_full/vov3d", lh5_file, idx=[0, 10_000])
+    assert isinstance(lh5_obj, types.VectorOfVectors)
+    assert lh5_obj == types.VectorOfVectors([[[1, 2], [3, 4, 5]]])
+    assert len(lh5_obj) == 1
+
+    # A fully out-of-range idx should yield an empty object.
+    lh5_obj = store.read("/data/struct_full/vov3d", lh5_file, idx=[10_000])
+    assert isinstance(lh5_obj, types.VectorOfVectors)
+    assert len(lh5_obj) == 0
 
 
 def test_read_voev(lh5_file):
