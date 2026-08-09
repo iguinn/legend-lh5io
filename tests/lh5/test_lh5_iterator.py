b138c67bfedf9b3902e157ca4c8e2a58f009492d
iguinn <guinnis@ornl.gov>
2026-08-09 07:33:22 -0700

    Appease the pre-commit bot

--- tests/lh5/test_lh5_iterator.py
+++ tests/lh5/test_lh5_iterator.py
@@ -827,22 +827,20 @@ def test_map(more_lgnd_files):
     assert all(tb == tb_mp for tb, tb_mp in zip(lh5_it, map_mp, strict=True))
 
     # test multithreading
-    with ThreadPoolExecutor(2) as thread_pool:
-        with MapProgress(2, thread_pool) as prog:
-            map_mt = lh5_it.map(
-                return_tb, processes=2, executor=thread_pool, progress_queue=prog.queue
-            )
-            assert all(tb == tb_mt for tb, tb_mt in zip(lh5_it, map_mt, strict=True))
+    with ThreadPoolExecutor(2) as thread_pool, MapProgress(2, thread_pool) as prog:
+        map_mt = lh5_it.map(
+            return_tb, processes=2, executor=thread_pool, progress_queue=prog.queue
+        )
+        assert all(tb == tb_mt for tb, tb_mt in zip(lh5_it, map_mt, strict=True))
 
     # test multiprocessing
-    with ProcessPoolExecutor(2) as process_pool:
-        with MapProgress(2, process_pool) as prog:
-            map_mp = lh5_it.map(
-                return_tb, processes=2, executor=process_pool, progress_queue=prog.queue
-            )
-            assert all(tb == tb_mp for tb, tb_mp in zip(lh5_it, map_mp, strict=True))
+    with ProcessPoolExecutor(2) as process_pool, MapProgress(2, process_pool) as prog:
+        map_mp = lh5_it.map(
+            return_tb, processes=2, executor=process_pool, progress_queue=prog.queue
+        )
+        assert all(tb == tb_mp for tb, tb_mp in zip(lh5_it, map_mp, strict=True))
 
-    # TODO: once numpy figures out InterpreterPoolProcessor, add a test for intepreter pool
+    # TODO: once numpy figures out InterpreterPoolProcessor, add a test for interpreter pool
 
 
 def query_lgdo(tb, _):
